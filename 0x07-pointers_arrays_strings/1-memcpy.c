532b8a50f3aa0ad6ef2f5c8097b656a706ff6800
Abdelrahman <boda2642001@gmail.com>
2023-10-24 06:07:04 -0700

    0

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,16 +4,15 @@
  *   *@dest: the address of memory to print
  *    *@src: second value
  *     *@n: thirth value
- *      *
- *       * Return: char with result of memset
- *        */
+ **
+ ** Return: char with result of memset
+ **/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-		unsigned int i;
-
-			for (i = 0; i < n; i++)
-					{
-								dest[i] = src[i];
-									}
-				return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
